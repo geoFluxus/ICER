3a0c6cf695ed075a791ba1c7a803c82f5fb76cc5
Rusne Sileryte <rusne.sileryte@gmail.com>
2022-08-24 10:12:40 +0200

    deep analysis adjustments

--- indicator1.py
+++ indicator1.py
@@ -135,14 +135,20 @@ lokale_winning_cols = ['Land- en tuinbouwproducten',
                        'Ertsen',
                        'Zout, zand, grind, klei']
 
-
-goal = 'dmc_abiotic'
+# TO VISUALISE
+# goal = 'dmc_abiotic'
 # goal = 'dmi_abiotic'
-# goal = 'all_per_type'
-# goal = 'all_per_province'
-# goal = 'all'
 # goal = 'dmc_all'
 # goal = 'dmi_all'
+
+# TO ANALYSE
+# goal = 'all'  # output of all data after the analysis, per province, per product
+# goal = 'agg_per_type' # output of all data aggregated per biotic/abiotic type
+# goal = 'agg_per_province'  # output of all data aggregated per province
+
+goal = 'analysis'  # deeper analysis of a single province
+province = 'Zuid-Holland'
+
 
 
 dmcs = pd.DataFrame()
@@ -177,7 +183,7 @@ for sheet in years.keys():
     if 'abiotic' in goal:
         abiotic = data[data['resource'] == 'abiotic']
 
-        # TO DO -> add some ratio to mixed categories and include them in the calculation
+        # there are only two mixed categories, assuming equal distribution
         abiotic_in_mixed = data[data['resource'] == 'mixed']
         abiotic_in_mixed = abiotic_in_mixed.apply(lambda x: x * 0.5 if x.dtype == 'float64' else x)
 
@@ -186,11 +192,11 @@ for sheet in years.keys():
         aggregated = all_abiotic.groupby(['Provincie']).sum().reset_index()
 
     # aggregate per resource type biotic/abiotic/mixed
-    elif goal == 'all_per_type':
+    elif goal == 'agg_per_type':
         aggregated = data.groupby(['Provincie', 'resource']).sum().reset_index()
 
     # aggregated per province
-    elif goal == 'all_per_province':
+    elif goal == 'agg_per_province':
         aggregated = data.groupby(['Provincie']).sum().reset_index()
 
     # not aggregated at all
@@ -219,9 +225,9 @@ if 'dmi' in goal:
 if 'dmc' in goal:
     dmcs.to_excel(f'Private_data/{goal}.xlsx')
 if 'all' in goal:
-    all_data.to_excel('Private_data/aggregated_product_data.xlsx')
+    all_data.to_excel('Private_data/all_data.xlsx')
 
-# print(all_data)
+print(all_data)
 
 # draw visualisation
 if 'dmi' in goal:
@@ -230,18 +236,24 @@ if 'dmi' in goal:
 elif 'dmc' in goal:
     viz_data = dmcs
     val = 'DMC'
+else:
+    viz_data = pd.DataFrame
 
-# print(viz_data)
-viz_data = viz_data.groupby(['Provincie', 'year']).sum().reset_index()
-# print(viz_data)
 
-if True:
+if not viz_data.empty:
+
+    viz_data = viz_data.groupby(['Provincie', 'year']).sum().reset_index()
+    print(viz_data)
+
     sns.set()
     fig = sns.FacetGrid(data=viz_data, col='Provincie', hue='Provincie', aspect=0.5, height=5, col_wrap=6)
     fig.set(xlim=(2015, 2030)) #, ylim=(0,80000))
 
     # print(sns.regplot(x='year', y='DMI', data=viz_data))
-    df = regression(sns.regplot, "year", val, truncate=False)
+    # results = regression(sns.regplot, "year", val, truncate=False)
+
+    # print(results)
+
     fig.map(sns.regplot, "year", val, truncate=False)
     # fig.add_legend()
 
@@ -253,50 +265,51 @@ if True:
 
 # ############### DEEPER ANALYSIS #################
 
-# province = 'Zuid-Holland'
-# value = 'DMC'
-#
-# prov_data = all_data[all_data['Provincie'] == province]
-#
-# # all product groups plotted
-# product_data = prov_data[['Goederengroep', value, 'year']]
-# product_data.columns = ['GG', 'val', 'year']
+if 'analysis' in goal:
 
-# # visualisation
-# if False:
-#     sns.set()
-#     fig = sns.FacetGrid(data=product_data, col='GG', hue='GG', col_wrap=8)
-#     fig.set(xlim=(2015, 2020))
-#
-#     fig.map(sns.regplot, "year", "val", truncate=False)
-#     # fig.add_legend()
-#
-#     plt.show()
-#
+    value = 'DMC'
 
-# product = 'Steenkool, bruinkool, aardgas en ruwe aardolie'
-# # all trade measures plotted
-#
-# trade_data = prov_data[prov_data['Goederengroep'] == product]
-#
-# trade_data = trade_data.melt(id_vars=['year'], value_vars=['Invoer_nationaal',
-#                                                            'Invoer_internationaal',
-#                                                            'Aanbod',
-#                                                            'Uitvoer_nationaal',
-#                                                            'Uitvoer_internationaal',
-#                                                            'Winning',
-#                                                            'DMI',
-#                                                            'DMC'])
-#
-# print(trade_data)
-#
-# # visualisation
-# if False:
-#     sns.set()
-#     fig = sns.FacetGrid(data=trade_data, col='variable', hue='variable', col_wrap=8)
-#     fig.set(xlim=(2015, 2020))
-#
-#     fig.map(sns.regplot, "year", "value", truncate=False)
-#     # fig.add_legend()
-#
-    # plt.show()
+    prov_data = all_data[all_data['Provincie'] == province]
+
+    # all product groups plotted
+    product_data = prov_data[['cbs', value, 'year']]
+    product_data.columns = ['GG', 'val', 'year']
+
+    # visualisation
+    if True:
+        sns.set()
+        fig = sns.FacetGrid(data=product_data, col='GG', hue='GG', col_wrap=8)
+        fig.set(xlim=(2015, 2020))
+
+        fig.map(sns.regplot, "year", "val", truncate=False)
+        # fig.add_legend()
+
+        plt.show()
+
+
+    product = 'Steenkool, bruinkool, aardgas en ruwe aardolie'
+    # all trade measures plotted
+
+    trade_data = prov_data[prov_data['Goederengroep'] == product]
+
+    trade_data = trade_data.melt(id_vars=['year'], value_vars=['Invoer_nationaal',
+                                                               'Invoer_internationaal',
+                                                               'Aanbod',
+                                                               'Uitvoer_nationaal',
+                                                               'Uitvoer_internationaal',
+                                                               'Winning',
+                                                               'DMI',
+                                                               'DMC'])
+
+    print(trade_data)
+
+    # visualisation
+    if True:
+        sns.set()
+        fig = sns.FacetGrid(data=trade_data, col='variable', hue='variable', col_wrap=8)
+        fig.set(xlim=(2015, 2020))
+
+        fig.map(sns.regplot, "year", "value", truncate=False)
+        # fig.add_legend()
+
+        plt.show()
